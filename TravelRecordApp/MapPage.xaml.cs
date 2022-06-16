a6eb52cc6be8b836ca22f65c6f11941b263d58ad
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-06-16 16:58:33 +0300

    map

--- TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/MapPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xamarin.Essentials;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -15,6 +16,42 @@ namespace TravelRecordApp
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckAndRequestLocationPermission();
+
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+
+                locationsMaps.IsShowingUser = true;
+            }
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                //promt user to turn on the permission in settings
+                return status;
+            }
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
         }
     }
 }
